2f0578e4bb6628270705c36ef5defa83c09e3bad
Will Luer <will.luer@gmail.com>
2023-03-19 20:38:55 -0400

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -15,5 +15,31 @@ export type GameState = {
   name: string;
   isOver: boolean;
   winner: string;
-  state: string[];
+  state: string[][];
+};
+
+export type PieceProps = {
+  quadrant: number;
+  position: number;
+  color: string;
+  addPiece?: (quadrant: number, position: number) => void;
+};
+
+export type QuadrantProps = {
+  quadrant: number;
+  pieces: PieceProps[];
+  addPiece?: (quadrant: number, position: number) => void;
+  rotateQuadrant?: (quadrant: number, direction: string) => void;
+};
+
+export type BoardProps = {
+  quadrants: QuadrantProps[];
+  addPiece?: (quadrant: number, position: number) => void;
+  rotateQuadrant?: (quadrant: number, direction: string) => void;
+};
+export type RotationButtonProps = {
+  quadrant: number;
+  direction: string;
+  disabled: boolean;
+  rotateQuadrant?: (quadrant: number, direction: string) => void;
 };
