f0970604f962893b33cb91a21164fda5cb9f7d69
Alex Kim <dev@ajpkim.com>
2023-03-27 19:00:19 -0400

    Update game representation to explicitly include quadrants

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,47 +1,47 @@
-import { type } from "os";
+import { type } from "os"
 
 export type PieceMove = {
-  name: string;
-  quadrant: number;
-  position: number;
-};
+  name: string
+  quadrant: number
+  position: number
+}
 
 export type RotationMove = {
-  name: string;
-  quadrant: number;
-  direction: number;
-};
+  name: string
+  quadrant: number
+  direction: string
+}
 
-export type GameState = {
-  name: string;
-  turn: string;
-  isOver: boolean;
-  winner: string;
-  state: string[];
-};
+export type GameData = {
+  id: number
+  name: string
+  quadrants: string[]
+  turn: string
+  winner: string
+}
 
 export type PieceProps = {
-  quadrant: number;
-  position: number;
-  color: string;
-  addPiece?: (quadrant: number, position: number) => void;
-};
+  quadrant: number
+  position: number
+  color: string
+  addPiece?: (quadrant: number, position: number) => void
+}
 
 export type QuadrantProps = {
-  quadrant: number;
-  pieces: PieceProps[];
-  addPiece?: (quadrant: number, position: number) => void;
-  rotateQuadrant?: (quadrant: number, direction: string) => void;
-};
+  quadrant: number
+  pieces: PieceProps[]
+  addPiece?: (quadrant: number, position: number) => void
+  rotateQuadrant?: (quadrant: number, direction: string) => void
+}
 
 export type BoardProps = {
-  quadrants: QuadrantProps[];
-  addPiece?: (quadrant: number, position: number) => void;
-  rotateQuadrant?: (quadrant: number, direction: string) => void;
-};
+  quadrants: QuadrantProps[]
+  addPiece?: (quadrant: number, position: number) => void
+  rotateQuadrant?: (quadrant: number, direction: string) => void
+}
 export type RotationButtonProps = {
-  quadrant: number;
-  direction: string;
-  disabled: boolean;
-  rotateQuadrant?: (quadrant: number, direction: string) => void;
-};
+  quadrant: number
+  direction: string
+  disabled: boolean
+  rotateQuadrant?: (quadrant: number, direction: string) => void
+}
